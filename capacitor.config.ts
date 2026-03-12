e464aad9740601eecda70ea3ba6999fe3067c057
Gourab Das <dgourab574@gmail.com>
2026-03-12 23:46:11 +0530

    switch to capacitor.config.json and remove ts config

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.utranscribe.app',
-  appName: 'U-Transcribe',
-  webDir: 'www'
-};
-
-export default config;
